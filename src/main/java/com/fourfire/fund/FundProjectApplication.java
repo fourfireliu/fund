443333674ce13bae59a041cf5811a2fb558a7b2d
liuyi <liuyi02@imdada.cn>
2021-11-14 21:09:08 +0800

    init code

---
+++ src/main/java/com/fourfire/fund/FundProjectApplication.java
@@ -0,0 +1,13 @@
+package com.fourfire.fund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundProjectApplication.class, args);
+	}
+
+}
